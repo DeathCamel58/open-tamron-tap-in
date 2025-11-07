09625f663c2f1d6c4df07b94cb026076c7c0832a
Dylan Corrales <deathcamel58@gmail.com>
2025-11-07 15:21:04 -0500

    Remove Settings Card wasn't used

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { SerialProvider } from './contexts/SerialContext';
 import { Terminal } from './components/Terminal';
-import { Settings } from './components/Settings';
 import { Lens } from './components/Lens';
 import { Adapter } from "./components/Adapter.tsx";
 
@@ -18,15 +17,6 @@ export default function App() {
           <div className="mx-auto h-full grid grid-cols-1 gap-6">
             <div className="flex flex-col h-full">
               <Adapter />
-
-              <div className="bg-white shadow-sm rounded-lg overflow-hidden">
-                <div className="px-6 py-4 border-b">
-                  <h2 className="text-lg font-semibold">Settings</h2>
-                </div>
-                <div className="flex-1 min-h-0">
-                  <Settings />
-                </div>
-              </div>
             </div>
           </div>
         </div>
